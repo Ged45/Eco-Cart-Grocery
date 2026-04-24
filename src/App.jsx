a59d5d10418566aa5c8248848491043e2244921a
Eyuel M. Gedlie <eyuel.mazengia@aastustudent.edu.et>
2026-04-24 14:26:23 +0300

    Products asset added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeShop from "./pages/home-shope";
+
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
-      <HomeShop/>
       {/* add ur pages here */}
       <Footer />
     </>
