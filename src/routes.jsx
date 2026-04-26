c33df58ea046047781abcf951b7bfd0dba2d9d02
Elsa2116 <elsialemayehu33@gmail.com>
2026-04-26 12:12:15 +0300

    last commit

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,21 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import HomeShop from "./pages/home-shop";
+import AboutPage from "./pages/about";
+import Categories from "./pages/categories";
 
 // Pages
-
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />, // Layout (Navbar + Footer)
     children: [
-     {
-        path: "/", 
-        element: <HomeShop />,
-      },
-      
-      
+      { index: true, element: <HomeShop /> },
+      { path: "shop", element: <HomeShop /> },
+      { path: "about", element: <AboutPage /> },
+      { path: "categories", element: <Categories /> },
     ],
   },
 ]);
