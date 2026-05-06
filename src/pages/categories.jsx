9ad1efbc4045c38714fb3ba8aabd99e039b0db28
Ged45 <125463997+Ged45@users.noreply.github.com>
2026-05-06 18:03:59 +0300

    fixed

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -20,7 +20,7 @@ export function Categories() {
   const categories = CATEGORIES.filter((c) => c !== 'All');
 
   return (
-    <div className="max-w-7xl mx-145 px-4 sm:px-6 lg:px-8 py-8">
+    <div className="max-w-7xl mx-32 px-4 sm:px-6 lg:px-8 py-8">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
